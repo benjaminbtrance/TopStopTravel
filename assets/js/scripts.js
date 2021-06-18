d7f3c6ebf5e9f996e744c7fe5bd91ae20b480bb6
benjaminbtrance <benjaminb.tran@gmail.com>
2021-06-17 20:06:42 -0500

    add id for search btn. add logic to get data from ticketmaster api

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,1 +1,40 @@
-console.log('its working !!!');
+var searchCityEl = document.querySelector('#city-search');
+var searchCityBtn = document.querySelector('#city-search-btn');
+
+var ticketmasterAPIKey = 'hEhL4sdCUANVnAj4AMyPUUR9qmmjMvXb';
+
+function handleSearchFormSubmit(event) {
+	event.preventDefault();
+
+	var citySearchInputVal = document.querySelector('#city-search').value;
+
+	if (!citySearchInputVal) {
+		console.error('You need a search input value!');
+		return;
+	}
+
+	getTicketMasterMusicEvents();
+}
+
+function getTicketMasterMusicEvents() {
+	var apiUrl =
+		'https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&city=Dallas&apikey=' +
+		ticketmasterAPIKey;
+
+	fetch(apiUrl)
+		.then(function (response) {
+			if (response.ok) {
+				response.json().then(function (data) {
+					console.log(data);
+				});
+			} else {
+				console.warn(response.statusText);
+			}
+		})
+		.catch(function (error) {
+			console.warn('Unable to connect to API');
+		});
+}
+
+// Click Event
+searchCityBtn.addEventListener('click', handleSearchFormSubmit);
