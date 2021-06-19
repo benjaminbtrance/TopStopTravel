86ba14cdb00676d2fdeb2514f8a47678154d9e85
Benny Trance <benjaminb.tran@gmail.com>
2021-06-19 13:12:33 -0500

    add createCard function to display data to html

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -8,95 +8,130 @@ var openWeatherAPIKey = 'bc2194bf2b678d6ec02f05146c48236e';
 
 // Handles Form Event
 function handleSearchFormSubmit(event) {
-	event.preventDefault();
+  event.preventDefault();
 
-	var citySearchInputVal = searchCityEl.value.trim();
+  var citySearchInputVal = searchCityEl.value.trim();
 
-	if (!citySearchInputVal) {
-		return;
-	} else {
-		searchCityEl.value = '';
-	}
+  if (!citySearchInputVal) {
+    return;
+  } else {
+    searchCityEl.value = '';
+  }
 
-	var splitCity = citySearchInputVal.split(' ');
-	var encodedCity = splitCity.join('%20');
+  var splitCity = citySearchInputVal.split(' ');
+  var encodedCity = splitCity.join('%20');
 
-	// Call API functions
-	getTicketMasterMusicEvents(encodedCity);
-	getWeatherForecast(encodedCity);
-	getTicketMasterSportEvents(encodedCity);
+  // Call API functions
+  getTicketMasterMusicEvents(encodedCity);
+  getWeatherForecast(encodedCity);
+  getTicketMasterSportEvents(encodedCity);
 }
 
 function getWeatherForecast(city) {
-	var apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" +
-		openWeatherAPIKey;
+  var apiURL =
+    'https://api.openweathermap.org/data/2.5/weather?q=' +
+    city +
+    '&APPID=' +
+    openWeatherAPIKey;
 
-	fetch(apiURL)
-		.then(function (response) {
-			if (response.ok) {
-				response.json().then(function (data) {
-					console.log(data);
-				});
-			} else {
-				console.warn(response.statusText);
-			}
-		})
-		.catch(function (error) {
-			console.warn('Unable to connect to API');
-		})
-
+  fetch(apiURL)
+    .then(function (response) {
+      if (response.ok) {
+        response.json().then(function (data) {
+          console.log(data);
+        });
+      } else {
+        console.warn(response.statusText);
+      }
+    })
+    .catch(function (error) {
+      console.warn('Unable to connect to API');
+    });
 }
 
-function createCard {
-	document.createElement('div')
+function createTicketMasterCard() {
+  var divColumn = document.createElement('div');
+  var divCallout = document.createElement('div');
+
+  divColumn.className = 'column';
+  divCallout.className = 'callout';
+
+  for (i = 1; i < 5; i++) {
+    var p = document.createElement('p');
+    p.setAttribute('class', `p${i}`);
+    divCallout.appendChild(p);
+  }
+
+  musicEventEl.appendChild(divColumn);
+  divColumn.appendChild(divCallout);
+
+  console.log(musicEventEl);
+}
+
+function createWeatherCard() {
+  var divColumn = document.createElement('div');
+  var divCallout = document.createElement('div');
+
+  divColumn.className = 'column';
+  divCallout.className = 'callout';
+
+  for (i = 1; i < 5; i++) {
+    var p = document.createElement('p');
+    p.setAttribute('class', `p${i}`);
+    divCallout.appendChild(p);
+  }
+
+  musicEventEl.appendChild(divColumn);
+  divColumn.appendChild(divCallout);
+
+  console.log(musicEventEl);
 }
 
 function getTicketMasterMusicEvents(city) {
-	var apiUrl =
-		'https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&city=' +
-		city +
-		'&apikey=' +
-		ticketmasterAPIKey;
+  var apiUrl =
+    'https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&city=' +
+    city +
+    '&apikey=' +
+    ticketmasterAPIKey;
 
-	fetch(apiUrl)
-		.then(function (response) {
-			if (response.ok) {
-				response.json().then(function (data) {
-					console.log(data);
-
-				});
-			} else {
-				console.warn(response.statusText);
-			}
-		})
-		.catch(function (error) {
-			console.warn('Unable to connect to API');
-		});
+  fetch(apiUrl)
+    .then(function (response) {
+      if (response.ok) {
+        response.json().then(function (data) {
+          console.log(data);
+          createCard();
+        });
+      } else {
+        console.warn(response.statusText);
+      }
+    })
+    .catch(function (error) {
+      console.warn('Unable to connect to API');
+    });
 }
 
 function getTicketMasterSportEvents(city) {
-	var apiUrl =
-		'https://app.ticketmaster.com/discovery/v2/events.json?classificationName=sport&city=' +
-		city +
-		'&apikey=' +
-		ticketmasterAPIKey;
+  var apiUrl =
+    'https://app.ticketmaster.com/discovery/v2/events.json?classificationName=sport&city=' +
+    city +
+    '&apikey=' +
+    ticketmasterAPIKey;
 
-	fetch(apiUrl)
-		.then(function (response) {
-			if (response.ok) {
-				response.json().then(function (data) {
-					console.log(data);
-					console.log("Sport API Working");
-				});
-			} else {
-				console.warn(response.statusText);
-			}
-		})
-		.catch(function (error) {
-			console.warn('Unable to connect to API');
-		});
+  fetch(apiUrl)
+    .then(function (response) {
+      if (response.ok) {
+        response.json().then(function (data) {
+          console.log(data);
+          console.log('Sport API Working');
+        });
+      } else {
+        console.warn(response.statusText);
+      }
+    })
+    .catch(function (error) {
+      console.warn('Unable to connect to API');
+    });
 }
-
 
 // Click Event
 searchForm.addEventListener('submit', handleSearchFormSubmit);
